f699101bd8f194e84fe924fb352aa7979bd68d8a
AtulJoshyy <atuljoshyy@gmail.com>
2021-12-08 11:11:52 +0530

    Updated Color Theme ✅

--- script.js
+++ script.js
@@ -12,16 +12,28 @@ const btnHold = document.querySelector('.btn--hold');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
-// PREDEFINED CONDITIONS
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
 
 // FUNCTIONS
+const init = function () {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+
+}
+init()
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
@@ -64,7 +76,7 @@ btnHold.addEventListener('click', function () {
             scores[activePlayer];
 
         // check score if player sore is >=100
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 30) {
             // END THE GAME
             playing = false;
             diceEl.classList.add('hidden');
@@ -80,14 +92,4 @@ btnHold.addEventListener('click', function () {
     }
 });
 
-btnNew.addEventListener('click', function () {
-    // rest all the scores 
-    score0El.textContent = 0;
-    score1El.textContent = 0;
-    diceEl.classList.add('hidden');
-    const scores = [0, 0];
-    let currentScore = 0;
-    let activePlayer = 0;
-    let playing = true;
-
-});
+btnNew.addEventListener('click', init);
